7ec37c213503edf04b937f2cdab30d54efacc35a
M J Stanway <m.j.stanway@alum.mit.edu>
2021-04-19 23:40:33 -0700

    add a home.nix

---
+++ nixpkgs/home.nix
@@ -0,0 +1,82 @@
+{ config, pkgs, lib, ... }:
+
+{
+  programs.home-manager.enable = true;
+
+  home = {
+    packages = with pkgs; [ tree ];
+    sessionVariables = {
+      PAGER = "less";
+      EDITOR = "nvim";
+      VISUAL = "nvim";
+    };
+    # file.".Xresources".source = ../.Xresources;
+  };
+
+  programs.bash = {
+    enable = true;
+    shelloptions = [ "autocd" "globstar" "extglob"];
+  };
+
+  programs.direnv = {
+    enable = true;
+    enableNixDirenvIntegration = true;
+  };
+
+  programs.fish = {
+    enable = true;
+    plugins = [
+      {
+        name = "foreign-env";
+        src = pkgs.fetchFromGitHub {
+          owner = "oh-my-fish";
+	  repo = "plugin-foreign-env";
+          rev = "dddd9213272a0ab848d474d0cbde12ad034e65bc";
+          sha256 = "00xqlyl3lffc5l0viin1nyp819wf81fncqyz87jx8ljjdhilmgbs";
+	};
+      }
+    ];
+    loginShellInit = ''
+      if test -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+        fenv source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+      end
+
+      if test -e /nix/var/nix/profiles/default/etc/profile.d/nix.sh
+        fenv source /nix/var/nix/profiles/default/etc/profile.d/nix.sh
+      end
+
+      eval (direnv hook fish)
+      '';
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "M J Stanway";
+    ignores = [ "*~" "*.swp" "*.pyc" "*.o" ".DS_Store" ];
+    extraConfig = {
+      core = {
+        editor = "nvim";
+      };
+      url = {
+        "git@github.com" = { insteadOf = "https://github.com/"; };
+        "git@gitlab.com" = { insteadOf = "https://gitlab.com/"; };
+      };
+      pull.rebase = true;
+    };
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    defaultKeymap = "vi";
+    dotDir = ".config/zsh";
+  };
+  
+  services = {
+    gpg-agent = {
+      enable = true;
+      enableSshSupport = true;
+    };
+
+  };
+}

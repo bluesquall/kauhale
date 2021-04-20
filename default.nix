56058efbbad29873fc9dd1e611a3213c0af5302b
M J Stanway <m.j.stanway@alum.mit.edu>
2021-04-19 21:07:01 -0700

    add missing ;

--- default.nix
+++ default.nix
@@ -16,12 +16,12 @@ let
     pkgs.xclip
   ];
 
-#  fish = import ./fish (with pkgs; { inherit makeWrapper symlinkJoin writeText; fish = pkgs.fish });
+#  fish = import ./fish (with pkgs; { inherit makeWrapper symlinkJoin writeText; fish = pkgs.fish; });
   fish = pkgs.fish;
   git = import ./git ({ inherit (pkgs) sources runCommand makeWrapper symlinkJoin writeTextFile; git = pkgs.git; });
   i3 = pkgs.i3;
   nvim = pkgs.nvim;
-  tmux = import ./tmux (with pkgs; { inherit makeWrapper symlinkJoin writeText; tmux = pkgs.tmux });
+  tmux = import ./tmux (with pkgs; { inherit makeWrapper symlinkJoin writeText; tmux = pkgs.tmux; });
   zsh = pkgs.zsh;
 
 in

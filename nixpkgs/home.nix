431b40a34fb69e13efc278b609a2b9181f70eb51
M J Stanway <m.j.stanway@alum.mit.edu>
2021-05-14 16:56:39 -0700

    put reduced i3 config in home.nix

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -25,6 +25,59 @@
 
     file = {
 
+      "i3.config" = {
+        target = ".config/i3/config";
+        text = ''
+set $mod Mod4
+font pango:monospace 8
+bindsym $mod+Return exec st -f "Deja Vu Sans Mono:size=13" $HOME/.nix-profile/bin/fish
+bindsym $mod+z exec SHELL=$HOME/.nix-profile/bin/fish cool-retro-term
+bindsym $mod+b exec firefox
+bindsym $mod+Shift+q kill
+bindsym $mod+d exec dmenu_run
+bindsym $mod+h split h
+bindsym $mod+v split v
+bindsym $mod+f fullscreen toggle
+bindsym $mod+Shift+r restart
+bindsym $mod+Shift+c reload
+bindsym $mod+Shift+e exec "i3-nagbar -t warning -m 'You pressed the exit shortcut. Do you really want to exit i3? This will end your X session.' -B 'Yes, exit i3' 'i3-msg exit'"
+set $ws1 "1"
+set $ws2 "2"
+set $ws3 "3"
+set $ws4 "4"
+set $ws5 "5"
+set $ws6 "6"
+set $ws7 "7"
+set $ws8 "8"
+set $ws9 "9"
+set $ws10 "10"
+bindsym $mod+1 workspace number $ws1
+bindsym $mod+2 workspace number $ws2
+bindsym $mod+3 workspace number $ws3
+bindsym $mod+4 workspace number $ws4
+bindsym $mod+5 workspace number $ws5
+bindsym $mod+6 workspace number $ws6
+bindsym $mod+7 workspace number $ws7
+bindsym $mod+8 workspace number $ws8
+bindsym $mod+9 workspace number $ws9
+bindsym $mod+0 workspace number $ws10
+bindsym $mod+Shift+1 move container to workspace number $ws1
+bindsym $mod+Shift+2 move container to workspace number $ws2
+bindsym $mod+Shift+3 move container to workspace number $ws3
+bindsym $mod+Shift+4 move container to workspace number $ws4
+bindsym $mod+Shift+5 move container to workspace number $ws5
+bindsym $mod+Shift+6 move container to workspace number $ws6
+bindsym $mod+Shift+7 move container to workspace number $ws7
+bindsym $mod+Shift+8 move container to workspace number $ws8
+bindsym $mod+Shift+9 move container to workspace number $ws9
+bindsym $mod+Shift+0 move container to workspace number $ws10
+bar {
+        status_command i3status
+}
+xinput set-prop (xinput list --name-only | grep Touchpad) 'libinput Tapping Enabled' 1
+        '';
+      };
+
       "nix.conf" = {
         target = ".config/nix/nix.conf";
         text = "experimental-features = nix-command flakes";
@@ -58,12 +111,6 @@ XTerm*selectToClipboard: true
 Ctrl Shift <Key>C: copy-selection(CLIPBOARD)
 Ctrl Shift <Key>V: insert-selection(CLIPBOARD)
         '';
-      };
-
-      "i3" = {
-        source = ../i3;
-        target = "./.config/i3";
-        recursive = true;
       };
 
       # The following are shell completions that *may* not be installed by the tools themselves.

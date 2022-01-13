5f8cb83e549af75277bf936fb19e2f4355c0d7aa
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2022-01-12 18:10:59 -0800

    add moar packages to HM flake

--- nixpkgs/flake.nix
+++ nixpkgs/flake.nix
@@ -14,7 +14,22 @@
       "flynn@encom" = homeManager.lib.homeManagerConfiguration {
         configuration = {pkgs, ...}: {
           programs.home-manager.enable = true;
-          home.packages = [ pkgs.hello ];
+          home = {
+            sessionVariables = {
+              PAGER = "less";
+              EDITOR = "nvim";
+              VISUAL = "nvim";
+            };
+            packages = with pkgs; [
+              curl
+              dejavu_fonts
+              inconsolata
+              less
+              podman
+              st # TODO: Include clipboard, scrollback, & solarized patches.
+              tree
+            ];
+          }; # configuration
         };
         system = "x86_64-linux";
         homeDirectory = "/home/flynn"; # TODO: infer

f5c0e907a1c2b7f3de852512a7c7d15a1fed8397
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2022-01-13 21:12:27 -0800

    rename `nixos`->`nixpkgs`; move `let` block up
    - match wider conventions
    
    - follow along with Wil T

--- os/flake.nix
+++ os/flake.nix
@@ -1,62 +1,77 @@
 # flake.nix
 {
-  description = "kauhale-NixOS";
+  description = "group of houses comprising a home";
 
   inputs = {
-    nixos.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     agenix = {
       url = "github:ryantm/agenix";
-      inputs.nixpkgs.follows = "nixos";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     home-manager = {
       url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixos";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     # utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
   };
 
-  outputs = inputs@{ self, nixos, agenix, home-manager }: {
+  # outputs = inputs@{ self, nixpkgs, agenix, home-manager, ... }: {
+  #           ^        ^ TODO why do some examples include `self`?
+  #           ^ TODO what does this `inputs@` mean?
 
-    nixosConfigurations = let
-      kahua = {
-        system = "x86_64-linux";
-        modules = [
-          ({ lib, pkgs, ... }: {
-            networking = {
-              networkmanager.enable = true;
-              wireless.enable = lib.mkForce false;
-              # ^because WPA Supplicant cannot run with NetworkManager
-            };
-            environment = {
-              systemPackages = with pkgs; [ age bash curl git less neovim tree zsh ];
-              
-            };
-          })
-        ];
+  outputs = { nixpkgs, agenix, home-manager, ... }:
+  let # Wil says to put a let block in so we can do all our pre-calculated stuff at the top. TODO clarify
+    lib = nixpkgs.lib;
+    kahua = { # foundation
+      system = "x86_64-linux"; # TODO explore ways to generalize, at least to arm64
+
+      pkgs = import nixpkgs {
+        inherit (kahua) system;
+        config.allowUnfree = true;
       };
-    in {
-      minimalIso = nixos.lib.nixosSystem {
+
+      modules = [
+        ({ lib, pkgs, ... }: {
+          networking = {
+            networkmanager.enable = true;
+            wireless.enable = lib.mkForce false;
+            # ^because WPA Supplicant cannot run with NetworkManager
+          };
+          environment = {
+            systemPackages = with pkgs; [ age bash curl git less neovim tree zsh ];
+          };
+        })
+      ];
+
+    };
+  in {
+    nixosConfigurations = {
+
+      minimalIso = lib.nixosSystem {
         inherit (kahua) system;
         modules = kahua.modules ++ [
-          "${nixos}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+          "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
         ];
       };
-      plasmaIso = nixos.lib.nixosSystem {
+
+      plasmaIso = lib.nixosSystem {
         inherit (kahua) system;
         modules = kahua.modules ++ [
-          "${nixos}/nixos/modules/installer/cd-dvd/installation-cd-graphical-plasma5.nix"
+          "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-plasma5.nix"
         ];
       };
-      kauhale = nixos.lib.nixosSystem {
+
+      kauhale = lib.nixosSystem {
         inherit (kahua) system;
         modules = kahua.modules ++ [
           home-manager.nixosModules.home-manager
           ./configuration.nix
         ];
       };
+
     };
   };
 }

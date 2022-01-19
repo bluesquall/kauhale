98c7242518e1d4836fcbd07c96f700d70e638e05
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2022-01-19 00:05:02 -0800

    squash

--- user/flynn/default.nix
+++ user/flynn/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, lib, home-manager, ... }:
 
 let
   USERNAME = "flynn";
--- user/flynn/home.nix
+++ user/flynn/home.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, lib, home-manager, ... }:
 
 {
   programs.home-manager.enable = true;

ba3e5e9f398cc9ad73956dccc87e1c0a8fbb908b
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2023-04-20 23:48:36 -0700

    update flake.nix using tabula-rasa as reference

--- flake.nix
+++ flake.nix
@@ -7,112 +7,95 @@
 
     rust-overlay.url = "github:oxalica/rust-overlay";
 
-    ragenix = {
-      url = "github:yaxitech/ragenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    agenix.url = "github:yaxitech/ragenix";
+    agenix.inputs.nixpkgs.follows = "nixpkgs";
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, rust-overlay, ragenix, home-manager, ... }:
+  outputs = { nixpkgs, rust-overlay, agenix, home-manager, ... }:
   let # Wil says to put a let block in so we can do all our pre-calculated stuff at the top. TODO clarify
     lib = nixpkgs.lib;
+    system = "x86_64-linux"; # TODO explore ways to generalize, at least to arm64
+    overlays = [ rust-overlay.overlays.default agenix.overlays.default ];
 
-    kahua = { # foundation
+    pkgs = import nixpkgs {
+      inherit system overlays;
+      config.allowUnfree = true;
+    };
 
-      system = "x86_64-linux"; # TODO explore ways to generalize, at least to arm64
+    kahua = [
+      agenix.nixosModules.age
+      home-manager.nixosModules.home-manager {
+        home-manager.useGlobalPkgs = true;
+        # ^otherwise pure evaluation fails for flakes
+        home-manager.useUserPackages = true;
+      }
+      ({ lib, pkgs, ... }: {
+        nix = {
+          package = pkgs.nixUnstable;
+          extraOptions = "experimental-features = nix-command flakes recursive-nix";
+        };
 
-      pkgs = import nixpkgs {
-        inherit (kahua) system;
-        # config.allowUnfree = true;
-      };
+        networking = {
+          networkmanager.enable = true;
+          wireless.enable = lib.mkForce false;
+          # ^because WPA Supplicant cannot run with NetworkManager
+        };
+        programs.zsh.enable = true;
+        environment = {
+          shells = with pkgs; [ bash zsh ];
+          systemPackages = with pkgs; [ age agenix cryptsetup curl git less neovim tmux tree qrencode ];
+        };
+      })
+    ]; # kahua
 
-      modules = [
-        ragenix.nixosModules.age
+    kauhale = [
+      ({
+        nixpkgs.overlays = [
+          rust-overlay.overlays.default
+          agenix.overlays.default
+        ];
+      }) # <-- This looks redundant with the overlays inherited above. Is it necessary?
+    ]; # kauhale
 
-        ({ lib, pkgs, ... }: {
-
-          nix = {
-            package = pkgs.nixUnstable;
-            extraOptions = "experimental-features = nix-command flakes recursive-nix";
-          };
-
-          networking = {
-            networkmanager.enable = true;
-            wireless.enable = lib.mkForce false;
-            # ^because WPA Supplicant cannot run with NetworkManager
-          };
-
-          programs.zsh.enable = true;
-
-          environment = {
-            shells = with pkgs; [ bash zsh ];
-            systemPackages = with pkgs; [ age cryptsetup curl git less neovim tmux tree qrencode ];
-          };
-        })
-      ];
-    }; # kahua
-
-    kauhale = {
-      modules = [
-        home-manager.nixosModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          # ^otherwise pure evaluation fails for flakes
-          home-manager.useUserPackages = true;
-        }
-      ];
-    }; # kauhale
   in {
-
     homeConfigurations = {
       squall = home-manager.lib.homeManagerConfiguration {
-	      pkgs = nixpkgs.legacyPackages.${kahua.system};
-        modules = [ ./user/squall/home.nix ];
-	      # extraSpecialArgs = { inherit inputs outputs; };
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        modules = kauhale ++ [ ./user/squall/home.nix ];
+        # extraSpecialArgs = { inherit inputs outputs; };
       };
       "squall@echo" = home-manager.lib.homeManagerConfiguration {
-	      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        modules = [
-          ({ nixpkgs.overlays = [
-            rust-overlay.overlays.default
-            ragenix.overlays.default
-          ]; })
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        modules = kauhale ++ [
           ./user/squall/home.nix
           ./user/squall/echo.nix
         ];
-
-	      # extraSpecialArgs = { inherit inputs outputs; };
+        # extraSpecialArgs = { inherit inputs outputs; };
       };
     };
 
     nixosConfigurations = {
 
       iso = lib.nixosSystem {
-        inherit (kahua) pkgs system;
-        modules = kahua.modules ++ [
+        inherit pkgs system;
+        modules = kahua ++ [
           "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
         ];
       };
 
       plasmaIso = lib.nixosSystem {
-        inherit (kahua) pkgs system;
-        modules = kahua.modules ++ [
+        inherit pkgs system;
+        modules = kahua ++ [
           "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-plasma5.nix"
         ];
       };
 
       nimrod = lib.nixosSystem {
-        inherit (kahua) system;
-        modules = kahua.modules ++ kauhale.modules ++ [
-          ./os/nimrod
-          ./user/squall
-        ];
+        inherit pkgs system overlays;
+        modules = kahua ++ [ ./os/nimrod ./user/squall ];
       };
 
     };

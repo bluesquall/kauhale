a9e867c95feb0184d8377dd36cb5c6d194e4ea97
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2023-04-27 20:31:56 -0700

    add neovides to `nvims.zsh`

--- user/squall/nvims.zsh
+++ user/squall/nvims.zsh
@@ -1,7 +1,14 @@
+alias vim="NVIM_APPNAME=AstroNvim nvim"
+
 alias nvim-lazy="NVIM_APPNAME=LazyVim nvim"
 alias nvim-kick="NVIM_APPNAME=kickstart nvim"
 alias nvim-chad="NVIM_APPNAME=NvChad nvim"
 alias nvim-astro="NVIM_APPNAME=AstroNvim nvim"
+
+alias neovide-lazy="NVIM_APPNAME=LazyVim neovide"
+alias neovide-kick="NVIM_APPNAME=kickstart neovide"
+alias neovide-chad="NVIM_APPNAME=NvChad neovide"
+alias neovide-astro="NVIM_APPNAME=AstroNvim neovide"
 
 function nvims() {
   items=("default" "kickstart" "LazyVim" "NvChad" "AstroNvim")
@@ -13,6 +20,18 @@ function nvims() {
     config=""
   fi
   NVIM_APPNAME=$config nvim $@
+}
+
+function neovides() {
+  items=("default" "kickstart" "LazyVim" "NvChad" "AstroNvim")
+  config=$(printf "%s\n" "${items[@]}" | fzf --prompt=" Neovide Config  " --height=~50% --layout=reverse --border --exit-0)
+  if [[ -z $config ]]; then
+    echo "Nothing selected"
+    return 0
+  elif [[ $config == "default" ]]; then
+    config=""
+  fi
+  NVIM_APPNAME=$config neovide $@
 }
 
 bindkey -s ^a "nvims\n"

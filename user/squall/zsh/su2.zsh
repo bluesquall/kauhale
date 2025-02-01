1acfa53007866a745d5c2ffc0e47974fe2a3f170
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2025-02-01 13:39:34 -0800

    need to `export` environment variable `SU2_RUN`

--- user/squall/zsh/su2.zsh
+++ user/squall/zsh/su2.zsh
@@ -1,4 +1,4 @@
-su2_run=($HOME/.local/bin/su2/bin/)
+export SU2_RUN=$HOME/.local/bin/su2/bin/
 # TODO: revisit ^ to be less redundant, once I understand what's just in `bin` in the zip archive, and not also in `bin/su2`
 path=($SU2_RUN $path)
 export PATH

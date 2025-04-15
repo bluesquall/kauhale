8287047701cebfb76334e99f5399ccb03d4143d6
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2025-04-15 16:55:18 -0700

    add catpuccin for `tmux`

--- user/squall/tmux/tmux.nix
+++ user/squall/tmux/tmux.nix
@@ -7,6 +7,18 @@
     terminal = "tmux-256color";
     keyMode = "vi";
     clock24 = true;
+
+    plugins = with pkgs; [
+      {
+        plugin = tmuxPlugins.catppuccin;
+        extraConfig = ''
+          set -g @catppuccin_flavour 'frappe'
+          set -g @catppuccin_window_tabs_enabled on
+          set -g @catppuccin_date_time "%H:%M"
+        '';
+      }
+    ];
+
     extraConfig = "
 set -g allow-rename off
 set -g status-position top

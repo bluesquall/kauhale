434c826a2e89038c0f50064ecdd866458e1d93f4
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2025-04-16 12:55:25 -0700

    factor out sections of `tmux.conf` by functionality

--- user/squall/tmux/tmux.nix
+++ user/squall/tmux/tmux.nix
@@ -36,13 +36,9 @@
     ];
 
     extraConfig = ''
-set -g allow-rename off
-set -g status-position top
-
-set -gas terminal-overrides '*:Tc' # true color support
-set -gas terminal-overrides '*:RGB' # true color support
-set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'  # undercurl support
-set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'  # underscore colours - needs tmux-3.0
+      ${builtins.readFile ./ux.tmux.conf}
+      ${builtins.readFile ./true-color.tmux.conf}
+      ${builtins.readFile ./undercurl.tmux.conf}
     '';
   };
 }
